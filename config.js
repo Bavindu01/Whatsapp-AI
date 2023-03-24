1245b83998e048d6a10859ef32b4fce273f591e4
Bavindu01 <114155573+Bavindu01@users.noreply.github.com>
2023-03-24 20:27:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ const chalk = require('chalk')
 
 global.openaikey = process.env.OPEN_AI_KEY || "ENTER_API-KEY_HERE", //Put Open AI Api key
 	
-global.owner = process.env.OWNER || "94XXXXXXXX", //Bot Owner Number
+global.owner = process.env.OWNER || "94713378049", //Bot Owner Number
 	
-global.ownername = process.env.OWNER_NAME || "DarkWinzo", //Bot Owner Name
+global.ownername = process.env.OWNER_NAME || "theekshana", //Bot Owner Name
 	
 global.open_ai = process.env.OPEN_AI || "false", //Open AI Bot : "true" = on/"false" = off
 	
@@ -15,7 +15,7 @@ global.chatbot_pm = process.env.CHATBOT_PM || "true", //Inbox Msg Chat Bot : "tr
 	
 global.chatbot_group = process.env.OWNER || "true", //Group Msg Chat Bot : "true" = on/"false" = off
 	
-global.language = process.env.LANGUAGE || "EN", //EN < = English , SI < = Sinhala
+global.language = process.env.LANGUAGE || "SI", //EN < = English , SI < = Sinhala
 	
 global.bot_present = process.env.PRESENT || "false" //false is recommand..because it make crash
 
